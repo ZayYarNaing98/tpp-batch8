f640248c66d7c32c91f8b14884f50f0c74729132
ZayYarNaing <zayarnaing.pp@gmail.com>
2025-08-15 15:51:55 +0630

    Add Bootstarp UI in Cateogry

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -5,21 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Category Create</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div>
-        <h1>
-            + Category Create
-        </h1>
-
-        <form action="{{ route('categories.store') }}" method="POST">
-            @csrf
-            <input type="text" placeholder="Enter Category Name" name="name"/>
-            <button type="submit">
-                +Create
-            </button>
-            <a href="{{ route('categories.index') }}">Back</a>
-        </form>
+    <div class="container mt-4">
+        <div class="card">
+            <div class="card-header">
+                + Category Create
+            </div>
+            <form action="{{ route('categories.store') }}" method="POST">
+                @csrf
+                <div class="card-body">
+                    <input type="text" class="form-control" placeholder="Enter Category Name" name="name"/>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary btn-sm">
+                        + Create
+                    </button>
+                    <a href="{{ route('categories.index') }}" class="btn btn-secondary btn-sm ms-2">Back</a>
+                </div>
+            </form>
+        </div>
     </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
